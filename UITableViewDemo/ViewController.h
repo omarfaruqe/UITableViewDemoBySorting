a74fb3a9cef15932b965d4026cca5cde4c219ade
Omar Faruqe <faruqe.cse@gmail.com>
2016-01-17 17:50:10 -0500

    Initial Commit

---
+++ UITableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewDemo
+//
+//  Created by Omar Faruqe on 2016-01-17.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITableViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITableViewDemo
+//
+//  Created by Omar Faruqe on 2016-01-17.
+//  Copyright © 2016 Omar Faruqe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
